0e02daa645025889a919c0deda7412f8002f4350
Annie Phan <anniephan.atp@gmail.com>
2019-03-03 18:23:22 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,12 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
+    montague: {
       patriarch: {},
       matriarch: {},
       hero: {},
-      hero_friends:, []
+      hero_friends: []
       },
-    :capulet => {
+    capulet: {
       #your key/value pairs here!
       patriarch: {},
       matriarch: {},
